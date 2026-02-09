3830e4f4b18df519a93e720954da772553260ff9
Davide Pollicino <dpollicino@microsoft.com>
2026-02-09 18:54:30 +0100

    Add SHA256 for v1.0.0 release

--- Formula/facetimectl.rb
+++ Formula/facetimectl.rb
@@ -2,7 +2,7 @@ class Facetimectl < Formula
   desc "Fast CLI for FaceTime on macOS - make calls from the terminal"
   homepage "https://github.com/omonimus1/facetimectl"
   url "https://github.com/omonimus1/facetimectl/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "7a3eb5097d324d9df17301b57c6187c8957075729e6a88a2f9eb64d74f59bf39"
   license "MIT"
   head "https://github.com/omonimus1/facetimectl.git", branch: "master"
 
